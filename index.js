e49548ce0e9e59b0af758f27e227592391588af7
sahith007 <sahith007@gmail.com>
2024-10-18 15:43:41 +0000

    image fix, Hand draft

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const app = express();
 
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 
+var users = 6;
+
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   const boardData = JSON.parse(fs.readFileSync('./views/board_data.json', 'utf-8'));
-  res.render('index', { boardData });
+  res.render('index', { boardData, users });
 });
 
 app.get('/api', (req, res) => {
