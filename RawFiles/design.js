2885e11d0c34f8e54e08aed20669f75cd4089a8b
doraemon152003102 <doraemon152003102@gmail.com>
2024-10-28 18:07:30 +0000

    made changes

--- RawFiles/design.js
+++ RawFiles/design.js
@@ -1,7 +1,7 @@
 class Game{
     constructor(playerSize)
     {
-        this.board = new Board
+        this.board = new Board(10,10);
         this.playerSize=playerSize;
         this.b=1;
     }
@@ -39,9 +39,28 @@ class Board{
         if(x<this.n && y<this.m)
         this.board[x][y]=val;
     }
+    validateIntRow(row,size)
+    {
+        if(row.length==size)
+        {
+            let j=0;
+            while(j<size && Number.isInteger(row[j]))
+            j++;
+            return j==size;
+        }
+        else
+        return false;
+    }
+    setRow(x,row)
+    {
+        if(x<this.n && validateIntRow(row,this.m))
+        this.board[x]=row;
+    }
     
 }
 var myBoard= new Board(10,10);
-console.log(myBoard.getVal(9,9));
+var row = [1,2,3];
+var ans=myBoard.validateIntRow(row,4);
+console.log(ans);
 
 
